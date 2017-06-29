19c9af3fef73247e5b851963076dd26f6db111fb
PataPon <PataPon@PataPataPon.local>
2017-06-30 02:22:38 +0800

    Initial commit

---
+++ src/main/java/testserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package testserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by PataPon on 2017/6/30.
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
